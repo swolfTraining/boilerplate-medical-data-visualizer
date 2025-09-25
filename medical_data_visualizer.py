c3581b18d1c32e6babf8dd5f5f6aa7fcad21529d
swolfTraining <stwolf1989@outlook.de>
2025-09-25 08:57:41 +0200

    Update medical_data_visualizer.py

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,31 +4,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv('medical_examination.csv')
 
 # 2
-df['overweight'] = None
+df['overweight'] = (df['weight'] / (df['height']/100)**2) // 25
+df.loc[df['overweight'] > 1, 'overweight'] = 1
 
 # 3
-
+df.loc[df['cholesterol'] <= 1, 'cholesterol'] = 0
+df.loc[df['cholesterol'] > 1, 'cholesterol'] = 1
+df.loc[df['gluc'] <= 1, 'gluc'] = 0
+df.loc[df['gluc'] > 1, 'gluc'] = 1
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = df.melt(['cardio'], ['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
-    
+    df_cat = df_cat.groupby(['cardio', 'variable', 'value'])['value'].count()
+    df_cat = df_cat.to_frame()
+    df_cat = df_cat.rename(columns={'value': 'total'})
+    df_cat = df_cat.reset_index()
 
     # 7
-
-
+    df_cat_plot = sns.catplot(df_cat, x = 'variable', y='total', col = 'cardio', hue= 'value', kind = 'bar')
 
     # 8
-    fig = None
-
+    fig = df_cat_plot.figure
 
     # 9
     fig.savefig('catplot.png')
@@ -38,22 +41,25 @@ def draw_cat_plot():
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[df['ap_lo'] <= df['ap_hi']]
+    df_heat = df_heat[df_heat['height'] >= df['height'].quantile(0.025)]
+    df_heat = df_heat[df_heat['height'] <= df['height'].quantile(0.975)]
+    df_heat = df_heat[df_heat['weight'] >= df['weight'].quantile(0.025)]
+    df_heat = df_heat[df_heat['weight'] <= df['weight'].quantile(0.975)]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
-
+    mask = np.zeros(corr.shape)
+    mask[np.triu_indices(len(mask))] = 1
+    mask = mask.astype(bool)
+    
     # 14
-    fig, ax = None
+    fig, ax = plt.subplots()
 
     # 15
-
-
+    ax = sns.heatmap(corr, mask=mask, annot=True, fmt='.1f')
 
     # 16
     fig.savefig('heatmap.png')
